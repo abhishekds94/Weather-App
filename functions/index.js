49cf3035d75d138de3587cdddc75a966287f90b9
Abhishek D S <abhishekds94@gmail.com>
2019-06-07 22:32:28 -0500

    error fix in declarations

--- functions/index.js
+++ functions/index.js
@@ -10,11 +10,16 @@ const functions = require('firebase-functions');
 //Instantiate the dialogflow client
 const app = dialogflow({ debug: true });
 
+//WelcomeIntent
+app.intent('Default Welcome Intent', (conv) => {
+    conv.ask(`Welcome to Weather App. May I know which city weather you would like to know?`)
+});
+
 //Image URLs
 const clearImg = 'http://tmglive.tv/wp-content/uploads/2017/06/Sunny-clear-weather.jpeg';
 const rainImg = 'https://www.scienceabc.com/wp-content/uploads/2015/05/Walking-in-Rain.jpg';
 const cloudsImg = 'https://www.theglobeandmail.com/resizer/BGOzSdJP1VNRXJ9GCYbW9-VYtAk=/1200x0/filters:quality(80)/arc-anglerfish-tgam-prod-tgam.s3.amazonaws.com/public/5EG236O7RVH4LKH7RRD4SR5NPI';
-const hazeImg = 'https://www.healthhub.sg/sites/assets/Assets/Article%20Images/respiratory_haze.jpg?Width=970&Height=405',;
+const hazeImg = 'https://www.healthhub.sg/sites/assets/Assets/Article%20Images/respiratory_haze.jpg?Width=970&Height=405';
 const mistImg = 'https://graffitiwallpaper.com/pics/listings/139_wide.jpg';
 const thunderstormImg = 'http://images.skymetweather.com/content/wp-content/uploads/2016/04/Thundershowers-2.jpg';
 const drizzleImg = 'http://www.whitneydrake.com/wp-content/uploads/2012/11/drizzle-11292012.jpg';
